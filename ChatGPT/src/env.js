8abc2ecf108782f558e95c05660422ff74cbb405
lzm <776853031@qq.com>
2023-03-16 22:42:21 +0800

    add stream方式输出

--- ChatGPT/src/env.js
+++ ChatGPT/src/env.js
@@ -1,5 +1,5 @@
 export const env = {
   apiURL: 'https://api.openai.com/v1/chat/completions',
-  OPENAI_API_KEY: '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',
+  OPENAI_API_KEY: '',
   proxy: {}
 }
