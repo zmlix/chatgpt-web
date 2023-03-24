39f1b7101df1fe4581b93f3182f0ca744d79a3d2
lzm <776853031@qq.com>
2023-03-25 02:44:44 +0800

    fix bugs

--- ChatGPT/src/components/StopRequest.vue
+++ ChatGPT/src/components/StopRequest.vue
@@ -11,10 +11,10 @@ const stop = () => {
 <template>
   <div
     v-show="messagesStore.sending.isSending"
-    class="flex items-center justify-center w-full h-8 mt-3 fixed bottom-20"
+    class="flex items-center justify-center w-full h-0 relative bottom-6"
   >
     <div
-      class="flex items-center justify-center w-28 h-full bg-indigo-500 rounded text-white hover:cursor-pointer active:bg-indigo-600"
+      class="flex items-center justify-center w-28 h-8 bg-indigo-500 rounded text-white hover:cursor-pointer active:bg-indigo-600"
     >
       <div class="w-3 h-3 mr-1 border-2 border-white"></div>
       <span class="m-1 text-sm font-sans" @click="stop">停止请求</span>
